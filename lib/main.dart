c536e5ec98e646efe4bdc8a3e90364ed9257ae5d
David Riyan Kurniawan <SirilCommunityUINRIL23@gmail.com>
2023-12-08 22:58:49 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('testing network image to real device'),
+          ),
+          body: Center(
+              child: Image.network(
+            'https://i.pinimg.com/originals/c7/fa/89/c7fa89705d3397b653e808884c56b791.jpg',
+            loadingBuilder: (BuildContext context, Widget child,
+                ImageChunkEvent? loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              } else {
+                return CircularProgressIndicator(
+                  value: loadingProgress.expectedTotalBytes != null
+                      ? loadingProgress.cumulativeBytesLoaded /
+                          (loadingProgress.expectedTotalBytes ?? 1)
+                      : null,
+                );
+              }
+            },
+          )),
+        ));
+  }
+}
